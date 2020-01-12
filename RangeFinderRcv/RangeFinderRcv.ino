664586bcde4000f7b4154d34e0a38835ebf45c11
walteis <walt.eis@gmail.com>
2020-01-11 22:58:43 -0500

    Add files via upload

--- RangeFinderRcv/RangeFinderRcv.ino
+++ RangeFinderRcv/RangeFinderRcv.ino
@@ -1,7 +1,7 @@
 /*********************************************************************
-Program to display output from Ultrasonic range finder on a .96" OLED display.
+Program to display output from Ultrasonic range finder, sent by 433MHz transmitter, on a .96" OLED display.
 
-As designed, uses .96" display and Make "Maker" board.
+As designed uses .96" display and Make "Maker" board, heterodyne receiver, and UNO R3.
 
 Pinouts are:
 Display     Prototype board
@@ -11,11 +11,16 @@ Display     Prototype board
  SCL        A5 (SCL)
  SDA        A4 (SDA)
 
- Receiver       Prototype board
- ------------   ---------------
+Receiver   Prototype board
+--------   ---------------
  VCC            5v
  GND            GND
- DATA           D11
+ Data           D11
+
+
+
+
+
  
 *********************************************************************/
 #include <RH_ASK.h>
@@ -28,20 +33,13 @@ Display     Prototype board
 #include <Fonts/FreeSerif9pt7b.h>
 #include <Fonts/FreeMonoOblique9pt7b.h>
 
+
 // set constants for display
 #define OLED_RESET 4
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16
 
-#if (SSD1306_LCDHEIGHT != 32)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
-
-// preform display reset
+// preform reset
 Adafruit_SSD1306 display(OLED_RESET);
 
-// initialize range finder
-SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
 String a;   // global to hold distance
 
 RH_ASK driver;
@@ -56,7 +54,7 @@ void setup()   {
   if (!driver.init())
       Serial.println("init failed");
   
-  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
   
   // Clear the buffer.
@@ -67,10 +65,12 @@ void setup()   {
   display.setTextColor(WHITE);
   display.println("Range Finder v3.0");
   
-  // write heading line 2
+  // write column headings
+  display.setTextSize(1);
   display.setTextColor(BLACK, WHITE);
   display.setCursor(1,10);
   display.print("     Distance     ");
+  //display.setTextSize(2);
   display.setFont(&FreeMonoOblique9pt7b);
   display.display();
 }
@@ -79,32 +79,40 @@ void setup()   {
  *  Processing Loop
  */
 void loop() {
- 
+
+  
   String message;
- 
   // Clear last distance
-  // I do this to reset the screen to background color without a full reset.
+  // I do this to reset the screen to background color.
   display.setTextColor(BLACK,WHITE);
   display.setCursor(7,31);
   display.print(a);
+
   
-  // receive from TX
+  // get rangefinder output
+
   uint8_t buf[12];
   uint8_t buflen = sizeof(buf);
   if (driver.recv(buf, &buflen)) // Non-blocking
-  {
+    {
       int i;
       // Message with a good checksum received, dump it.
       a = (char*)buf;
       a = a.substring(0,a.length()-4);
       a.trim();
       a.concat(" in");
-  } 
+    } else
+    { 
+      display.print("No Link");
+    }
          
-  // display length as recieved from transmitter
+  // refresh the display
   display.setTextColor(WHITE, BLACK);
   display.setCursor(7,31);
   display.print(a);
+  Serial.println(message);
+
   display.display();
+  delay(10);
   
 }
